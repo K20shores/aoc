a4a9431e6ea063b76b0105dce008fa60f03b74cb
Kyle Shores <kyle.shores44@gmail.com>
2024-12-06 23:35:01 -0600

    day 6 part 1

---
+++ 2024/src/day6.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <benchmark/benchmark.h>
+
+struct Pos {
+  int y = 0;
+  int x = 0;
+
+  Pos& operator+(const Pos other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+  }
+  
+  Pos& operator+=(const Pos& other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+  }
+};
+
+struct Data {
+  std::vector<std::string> map;
+  Pos p;
+};
+
+Pos up = {-1, 0};
+Pos down = {1, 0};
+Pos left = {0, -1};
+Pos right = {0, 1};
+
+Pos next_dir(Pos original) {
+  return Pos{.y = original.x, .x = -original.y};
+}
+
+char get_next_grid_char(const std::vector<std::string>& map, const Pos& p, const Pos& dir) {
+  if (p.y + dir.y < map.size() && p.x + dir.x < map[0].size()) {
+    return map[p.y + dir.y][p.x + dir.x];
+  }
+  return '\0';
+}
+
+int part1(const Data &data)
+{
+  auto map = data.map;
+  Pos p = data.p;
+  Pos dir;
+  char c = map[p.y][p.x];
+  switch(c) {
+    case '^':
+      dir = up;
+      break;
+    case 'v':
+      dir = down;
+      break;
+    case '>':
+      dir = right;
+      break;
+    case '<':
+      dir = left;
+      break;
+  }
+
+  while (true) {
+    char next = get_next_grid_char(map, p, dir);
+    map[p.y][p.x] = 'X';
+    if (next == '\0') break;
+    if (next == '#') {
+      dir = next_dir(dir);
+    }
+    p += dir;
+  }
+
+  int sum = 0;
+  for(auto& row : map) {
+    for(auto & c : row) {
+      if (c == 'X') sum += 1;
+    }
+  }
+  return sum;
+}
+
+int part2(const Data &data)
+{
+  return 0;
+}
+
+Data parse()
+{
+  std::ifstream file(std::filesystem::path("inputs/day6.txt"));
+  if (!file.is_open())
+  {
+    throw std::runtime_error("file not found");
+  }
+  std::string line;
+  Data data;
+
+  size_t i = 0;
+  while (std::getline(file, line))
+  {
+    data.map.push_back(line);
+    auto p1 = line.find('^');
+    auto p2 = line.find('v');
+    auto p3 = line.find('>');
+    auto p4 = line.find('<');
+    auto a = {p1, p2, p3, p4};
+    for(auto& p : a) {
+      if (p != std::string::npos) {
+        data.p.y = i;
+        data.p.x = p;
+        break;
+      }
+    }
+    ++i;
+  }
+
+  return data;
+}
+
+class BenchmarkFixture : public benchmark::Fixture
+{
+public:
+  static Data data;
+};
+
+Data BenchmarkFixture::data = parse();
+
+BENCHMARK_DEFINE_F(BenchmarkFixture, Part1Benchmark)
+(benchmark::State &state)
+{
+  for (auto _ : state)
+  {
+    int s = part1(data);
+    benchmark::DoNotOptimize(s);
+  }
+}
+
+BENCHMARK_DEFINE_F(BenchmarkFixture, Part2Benchmark)
+(benchmark::State &state)
+{
+  for (auto _ : state)
+  {
+    int s = part2(data);
+    benchmark::DoNotOptimize(s);
+  }
+}
+
+BENCHMARK_REGISTER_F(BenchmarkFixture, Part1Benchmark);
+BENCHMARK_REGISTER_F(BenchmarkFixture, Part2Benchmark);
+
+int main(int argc, char **argv)
+{
+  Data data = parse();
+
+  int answer1 = 5312;
+  int answer2 = 0;
+
+  auto first = part1(data);
+  auto second = part2(data);
+
+  std::cout << "Part 1: " << first << std::endl;
+  std::cout << "Part 2: " << second << std::endl;
+
+  first != answer1 ? throw std::runtime_error("Part 1 incorrect") : nullptr;
+  second != answer2 ? throw std::runtime_error("Part 2 incorrect") : nullptr;
+
+  for (int i = 1; i < argc; ++i) {
+    if (std::string(argv[i]) == "--benchmark") {
+      benchmark::Initialize(&argc, argv);
+      benchmark::RunSpecifiedBenchmarks();
+      return 0;
+    }
+  }
+}
