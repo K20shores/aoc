7357f42e0fb9c0e0ab765c970183ccfa2256ea9c
Kyle Shores <kyle.shores44@gmail.com>
2024-12-11 21:20:23 -0600

    now it looks like I did 1 and 2 the smart way

--- 2024/src/day11.cpp
+++ 2024/src/day11.cpp
@@ -9,92 +9,12 @@
 #include <stack>
 #include <queue>
 
-struct Node
-{
-  Node *left;
-  Node *right;
-  uint64_t value;
-};
-
 struct Data
 {
   std::vector<int> stones;
 };
 
-Node *get_node(uint64_t new_value, std::map<uint64_t, std::unique_ptr<Node>> &splits)
-{
-  if (!splits.contains(new_value))
-  {
-    splits[new_value] = std::make_unique<Node>(Node{.left = nullptr, .right = nullptr, .value = new_value});
-  }
-  return splits[new_value].get();
-}
-
 uint64_t count_stones(int blinks, const Data &data)
-{
-  std::map<uint64_t, std::unique_ptr<Node>> splits;
-  uint64_t sum = 0;
-
-  for (auto &stone : data.stones)
-  {
-    // stone value, depth
-    std::queue<std::pair<uint64_t, uint64_t>> q;
-    q.push({stone, 0});
-    sum += 1;
-
-    while (!q.empty())
-    {
-      auto val = q.front();
-      Node *cur = get_node(val.first, splits);
-
-      for (size_t blink = val.second; blink < blinks; ++blink)
-      {
-        while (cur->left != nullptr && blink < blinks)
-        {
-          if (cur->right != nullptr)
-          {
-            ++sum;
-            q.push({cur->right->value, blink + 1});
-          }
-          ++blink;
-          cur = cur->left;
-        }
-        if (blink >= blinks)
-          break;
-        uint64_t num_digits = std::floor(std::log10(cur->value)) + 1;
-        if (cur->value == 0)
-        {
-          Node *next = get_node(1, splits);
-          cur->left = next;
-          cur = cur->left;
-        }
-        else if (num_digits % 2 == 0)
-        {
-          uint64_t right_half = cur->value % uint64_t(std::pow(10, num_digits / 2));
-          uint64_t left_half = cur->value / std::pow(10, num_digits / 2);
-          sum += 1;
-          Node *left = get_node(left_half, splits);
-          Node *right = get_node(right_half, splits);
-          cur->left = left;
-          cur->right = right;
-          cur = cur->left;
-          q.push({right_half, blink + 1});
-        }
-        else
-        {
-          Node *next = get_node(cur->value * 2024, splits);
-          cur->left = next;
-          cur = cur->left;
-        }
-      }
-      q.pop();
-    }
-  }
-
-  return sum;
-}
-
-uint64_t count_stones_smartly(int blinks, const Data &data)
 {
   std::map<uint64_t, uint64_t> numbers;
   for (auto &stone : data.stones)
@@ -144,7 +64,7 @@ uint64_t part1(const Data &data)
 
 uint64_t part2(const Data &data)
 {
-  return count_stones_smartly(75, data);
+  return count_stones(75, data);
 }
 
 Data parse()
